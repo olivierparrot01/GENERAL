155a70138a2bba9ca08e3373436f309d80276873
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-07-31 18:37:01 +0200

    Update distance1.py

--- distance1.py
+++ distance1.py
@@ -5,7 +5,7 @@ import numpy as np
 
 # Load data from CSV file
 df1= pd.read_csv('https://raw.githubusercontent.com/olivierparrot01/ICPE/main/1geocodage.csv')              
-df=df1.loc[df1['result_sco']>0.5]
+df=df1.loc[float(df1['result_sco'])>0.5]
 # Define distance bins (up to 1000m in 100m intervals, then >1000m)
 distance_bins = list(range(0, 1100, 100)) + [np.inf]
 
