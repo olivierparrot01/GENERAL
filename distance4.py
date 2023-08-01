65be3638c11e2b7d62516cdacbcd519fb07d3c22
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-01 11:57:14 +0200

    Update distance4.py

--- distance4.py
+++ distance4.py
@@ -5,7 +5,7 @@ import base64
 
 # Load data from CSV
 df = pd.read_csv('https://raw.githubusercontent.com/olivierparrot01/ICPE/main/1geocodage.csv')
-
+df['Distance'] =df['Distance'].astype(int)
 # Define distance bins (100 m interval)
 distance_bins = list(range(0, 1100, 100))
 
