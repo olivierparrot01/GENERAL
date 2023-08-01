fbc138567cb7fbfc51883d95c75ec9d301b5c3fb
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-01 15:12:10 +0200

    Update distance5.py

--- distance5.py
+++ distance5.py
@@ -100,5 +100,5 @@ if st.button(f"Download Filtered Data for Interval {selected_interval_left} to {
     st.markdown(get_csv_download_link(filtered_df_statut_seveso_haut, f'df_statut_seveso_haut_interval_{selected_interval_left}_{selected_interval_right}'), unsafe_allow_html=True)
 
 if st.button(f"Download Filtered Data for Interval {selected_interval_left} to {selected_interval_right} (Seveso seuil bas)"):
-    filtered_df_statut_seveso_haut = filter_dataframe_by_interval(pd.Interval(selected_interval_left, selected_interval_right), 'Seveso seuil bas')
+    filtered_df_statut_seveso_bas = filter_dataframe_by_interval(pd.Interval(selected_interval_left, selected_interval_right), 'Seveso seuil bas')
     st.markdown(get_csv_download_link(filtered_df_statut_seveso_bas, f'df_statut_seveso_haut_interval_{selected_interval_left}_{selected_interval_right}'), unsafe_allow_html=True)
