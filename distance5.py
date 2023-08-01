a9103d209d43c971cd0e3bac8628c01c14861f9b
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-01 14:28:25 +0200

    Update distance5.py

--- distance5.py
+++ distance5.py
@@ -50,28 +50,40 @@ st.table(statut_seveso_haut_counts)
 st.write("Counts of 'Seveso seuil bas' in Each Distance Category")
 st.table(statut_seveso_bas_counts)
 
-# Create a function to filter DataFrame based on selected interval
-def filter_dataframe_by_interval(interval):
-    return df[df['Distance'].between(interval.left, interval.right)]
 # Create a function to convert DataFrame to CSV and get the link for download
 def get_csv_download_link(df, filename):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="{filename}.csv">Download {filename} CSV File</a>'
     return href
+
+# Add download links for each DataFrame
+st.markdown(get_csv_download_link(df[df['Statut_IED'] == 'Oui'], 'df_statut_ied_all'), unsafe_allow_html=True)
+st.markdown(get_csv_download_link(df[df['Statut_Sev'] == 'Seveso seuil haut'], 'df_statut_seveso_haut_all'), unsafe_allow_html=True)
+st.markdown(get_csv_download_link(df[df['Statut_Sev'] == 'Seveso seuil bas'], 'df_statut_seveso_bas_all'), unsafe_allow_html=True)
+
+# Create a function to filter DataFrame based on selected interval
+def filter_dataframe_by_interval(interval, statut):
+    if statut == 'Statut_IED':
+        return df[df['Distance'].between(interval.left, interval.right) & (df['Statut_IED'] == 'Oui')]
+    elif statut == 'Seveso seuil haut':
+        return df[df['Distance'].between(interval.left, interval.right) & (df['Statut_Sev'] == 'Seveso seuil haut')]
+    elif statut == 'Seveso seuil bas':
+        return df[df['Distance'].between(interval.left, interval.right) & (df['Statut_Sev'] == 'Seveso seuil bas')]
+
 # Add download links for the filtered DataFrames
-selected_interval = st.slider("Select an Interval:", min_value=100, max_value=1000, step=100)
+selected_interval = st.slider("Select an Interval:", min_value=0, max_value=len(distance_bins)-2, step=1)
 selected_interval_left = distance_bins[selected_interval]
 selected_interval_right = distance_bins[selected_interval + 1]
 
-if st.button(f"Download Filtered Data for Interval {selected_interval_left} to {selected_interval_right}"):
-    filtered_df_statut_ied = filter_dataframe_by_interval(pd.Interval(selected_interval_left, selected_interval_right))
+if st.button(f"Download Filtered Data for Interval {selected_interval_left} to {selected_interval_right} (Statut_IED)"):
+    filtered_df_statut_ied = filter_dataframe_by_interval(pd.Interval(selected_interval_left, selected_interval_right), 'Statut_IED')
     st.markdown(get_csv_download_link(filtered_df_statut_ied, f'df_statut_ied_interval_{selected_interval_left}_{selected_interval_right}'), unsafe_allow_html=True)
 
-if st.button(f"Download Filtered Data for Interval {selected_interval_left} to {selected_interval_right}"):
-    filtered_df_statut_seveso_haut = filter_dataframe_by_interval(pd.Interval(selected_interval_left, selected_interval_right))
+if st.button(f"Download Filtered Data for Interval {selected_interval_left} to {selected_interval_right} (Seveso seuil haut)"):
+    filtered_df_statut_seveso_haut = filter_dataframe_by_interval(pd.Interval(selected_interval_left, selected_interval_right), 'Seveso seuil haut')
     st.markdown(get_csv_download_link(filtered_df_statut_seveso_haut, f'df_statut_seveso_haut_interval_{selected_interval_left}_{selected_interval_right}'), unsafe_allow_html=True)
 
-if st.button(f"Download Filtered Data for Interval {selected_interval_left} to {selected_interval_right}"):
-    filtered_df_statut_seveso_bas = filter_dataframe_by_interval(pd.Interval(selected_interval_left, selected_interval_right))
+if st.button(f"Download Filtered Data for Interval {selected_interval_left} to {selected_interval_right} (Seveso seuil bas)"):
+    filtered_df_statut_seveso_bas = filter_dataframe_by_interval(pd.Interval(selected_interval_left, selected_interval_right), 'Seveso seuil bas')
     st.markdown(get_csv_download_link(filtered_df_statut_seveso_bas, f'df_statut_seveso_bas_interval_{selected_interval_left}_{selected_interval_right}'), unsafe_allow_html=True)
