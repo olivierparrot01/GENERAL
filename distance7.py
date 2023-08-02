f2851f3db25dbf66fc3b094d9eea6b23ed7798ba
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-02 10:01:45 +0200

    Update distance7.py

--- distance7.py
+++ distance7.py
@@ -34,7 +34,7 @@ def format_interval_label(interval_index):
 
 # Create a multiselect to choose multiple criteria to filter the DataFrame
 st.markdown("<h2 style='font-size:24px;'>Filtrer le fichier de géocodage</h2>", unsafe_allow_html=True)
-selected_criteria = st.multiselect("Sélectionner les critères:", options=['result_typ', 'result_sco'])
+selected_criteria = st.multiselect("", options=['result_typ', 'result_sco'])
 
 # Apply the selected criteria to filter the DataFrame
 filtered_df = df.copy()
