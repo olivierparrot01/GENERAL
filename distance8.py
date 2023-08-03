1ae29f8db276e7432386b29047b0d90773b17c0b
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-03 19:19:29 +0200

    Update distance8.py

--- distance8.py
+++ distance8.py
@@ -77,8 +77,12 @@ statut_seveso_bas_counts.index = statut_seveso_bas_counts.index.astype(str)
 
 
 # Show the table for distances
+
+
+codes_aiot = df[df['Code_AIOT'].notna()]['Code_AIOT'].astype(str).unique()
 st.markdown("<h2 style='font-size:18px;'>Nb ICPE de tout type par intervalle de distance en m (par rapport à GUN)</h2>", unsafe_allow_html=True)
 st.table(hist_data)
+
 st.markdown(f"Codes AIOT correspondants : {', '.join(df[df['Code_AIOT'].notna()]['Code_AIOT'].unique())}", unsafe_allow_html=True)
 st.markdown(get_csv_download_link(df[df['Code_AIOT'].notna()], "le fichier de géocodage correspondant"), unsafe_allow_html=True)
 
