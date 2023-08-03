2c4c55e78ee9e467c86c1a8d6d0a361ef77e8678
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-03 18:56:43 +0200

    Update distance8.py

--- distance8.py
+++ distance8.py
@@ -76,45 +76,46 @@ statut_seveso_bas_counts.index = statut_seveso_bas_counts.index.astype(str)
 
 
 
-
 # Show the table for distances
-# st.write("Nb ICPE tout type par intervalle de distance (par rapport à GUN)")
-
 st.markdown("<h2 style='font-size:18px;'>Nb ICPE de tout type par intervalle de distance en m (par rapport à GUN)</h2>", unsafe_allow_html=True)
-#selected_criteria = st.multiselect("", options=['result_typ', 'result_sco'])
-
-# Show the table for distances
-#st.markdown("<h2 style='font-size:18px;'>Nb ICPE de tout type par intervalle de distance en m (par rapport à GUN)</h2>", unsafe_allow_html=True)
 
 # Check if the user has selected the checkbox
-show_hist_data = st.checkbox("Afficher les données")
+show_hist_data = st.checkbox("Afficher les données de l'histogramme")
 
 if show_hist_data:
     st.table(hist_data)
-
-
-
-#st.table(hist_data)
-# Add download link for the filtered DataFrame
-st.markdown(get_csv_download_link(filtered_df, f'le fichier de geocodage correspondant'), unsafe_allow_html=True)
+    # Add download link for the filtered DataFrame
+    st.markdown(get_csv_download_link(filtered_df, "le fichier de géocodage correspondant"), unsafe_allow_html=True)
 
 # Show the table for 'Statut_IED' counts
 st.markdown("<h2 style='font-size:18px;'>Nb ICPE 'IED' par intervalle de distance en m (par rapport à GUN)</h2>", unsafe_allow_html=True)
 
-#st.write("Nb ICPE 'IED' par intervalle de distance (par rapport à GUN)")
-st.table(statut_ied_counts)
-st.markdown(get_csv_download_link(df[df['Statut_IED'] == 'Oui'], 'le fichier de geocodage correspondant'), unsafe_allow_html=True)
-# Show the table for 'Seveso seuil haut' counts
-st.markdown("<h2 style='font-size:18px;'>Nb ICPE 'Seveso seuil haut'  par intervalle de distance en m (par rapport à GUN)</h2>", unsafe_allow_html=True)
-#st.write("Nb ICPE 'Seveso seuil' haut par intervalle de distance (par rapport à GUN)")
-st.table(statut_seveso_haut_counts)
-st.markdown(get_csv_download_link(df[df['Statut_Sev'] == 'Seveso seuil haut'], 'le fichier de geocodage correspondant'), unsafe_allow_html=True)
-# Show the table for 'Seveso seuil bas' counts
-st.markdown("<h2 style='font-size:18px;'>Nb ICPE 'Seveso seuil bas'  par intervalle de distance en m (par rapport à GUN)</h2>", unsafe_allow_html=True)
+# Check if the user has selected the checkbox
+show_statut_ied_data = st.checkbox("Afficher les données 'Statut_IED'")
 
-#st.write("Nb ICPE 'Seveso seuil bas' par intervalle de distance (par rapport à GUN)")
-st.table(statut_seveso_bas_counts)
-st.markdown(get_csv_download_link(df[df['Statut_Sev'] == 'Seveso seuil bas'], 'le fichier de geocodage correspondant'), unsafe_allow_html=True)
+if show_statut_ied_data:
+    st.table(statut_ied_counts)
+    st.markdown(get_csv_download_link(df[df['Statut_IED'] == 'Oui'], "le fichier de géocodage correspondant"), unsafe_allow_html=True)
+
+# Show the table for 'Seveso seuil haut' counts
+st.markdown("<h2 style='font-size:18px;'>Nb ICPE 'Seveso seuil haut' par intervalle de distance en m (par rapport à GUN)</h2>", unsafe_allow_html=True)
+
+# Check if the user has selected the checkbox
+show_seveso_haut_data = st.checkbox("Afficher les données 'Seveso seuil haut'")
+
+if show_seveso_haut_data:
+    st.table(statut_seveso_haut_counts)
+    st.markdown(get_csv_download_link(df[df['Statut_Sev'] == 'Seveso seuil haut'], "le fichier de géocodage correspondant"), unsafe_allow_html=True)
+
+# Show the table for 'Seveso seuil bas' counts
+st.markdown("<h2 style='font-size:18px;'>Nb ICPE 'Seveso seuil bas' par intervalle de distance en m (par rapport à GUN)</h2>", unsafe_allow_html=True)
+
+# Check if the user has selected the checkbox
+show_seveso_bas_data = st.checkbox("Afficher les données 'Seveso seuil bas'")
+
+if show_seveso_bas_data:
+    st.table(statut_seveso_bas_counts)
+    st.markdown(get_csv_download_link(df[df['Statut_Sev'] == 'Seveso seuil bas'], "le fichier de géocodage correspondant"), unsafe_allow_html=True)
 
 
 # Create a function to filter DataFrame based on selected interval
