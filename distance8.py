f5cbb29869036249a91f63b4bc579deadcdf0310
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-04 17:10:53 +0200

    Update distance8.py

--- distance8.py
+++ distance8.py
@@ -144,7 +144,7 @@ selected_interval_right = distance_bins[selected_interval_index + 1]
 
 filtered_df_statut_ied = filter_dataframe_by_interval(pd.Interval(selected_interval_left, selected_interval_right), 'Statut_IED')
     #st.dataframe(filtered_df_statut_ied)
-with st.expander("Afficher/Masquer la table"):
+with st.expander(f"Afficher les données pour l'intervalle {selected_interval_left} à {selected_interval_right} (Statut_IED)"):
     # Afficher la table à l'intérieur de la section expansible
     st.table(filtered_df_statut_ied)
 
