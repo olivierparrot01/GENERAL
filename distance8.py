6c221a35fcdb410d4ff568461ce3a9029451aa32
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-04 16:19:04 +0200

    Update distance8.py

--- distance8.py
+++ distance8.py
@@ -74,13 +74,18 @@ statut_ied_counts.index = statut_ied_counts.index.astype(str)
 statut_seveso_haut_counts.index = statut_seveso_haut_counts.index.astype(str)
 statut_seveso_bas_counts.index = statut_seveso_bas_counts.index.astype(str)
 
-show_data = st.button("Afficher/Masquer les données")
-# Afficher les données si l'état d'affichage est True
-if show_data:
-    st.markdown("<h2 style='font-size:18px;'>Nb ICPE de tout type par intervalle de distance en m (par rapport à GUN)</h2>", unsafe_allow_html=True)
+# Créer une variable pour suivre l'état d'affichage de la table
+show_table = False
+
+# Bouton pour afficher/masquer la table
+if st.button("Afficher/Masquer la table"):
+    show_table = not show_table  # Inverser l'état d'affichage
+
+# Afficher la table si l'état d'affichage est True
+if show_table:
+    # Obtenez vos données ici et stockez-les dans la variable "table_data"
+   
     st.table(hist_data)
-else :
-    st.empty()
 
 # Show the table for 'Statut_IED' counts
 st.markdown("<h2 style='font-size:18px;'>Nb ICPE 'IED' par intervalle de distance en m (par rapport à GUN)</h2>", unsafe_allow_html=True)
