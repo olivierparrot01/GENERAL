fae291ff786f3967009882100f9dba50cc99b027
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-07 20:10:58 +0200

    Update distance8.py

--- distance8.py
+++ distance8.py
@@ -192,12 +192,12 @@ filtered_df = df[df['Code_AIOT'].isin(filtered_dg1['Code_AIOT'])]
 center_lat = filtered_dg1['latitude'].mean()
 center_lon = filtered_dg1['longitude'].mean()
 
-# Créer une seule carte avec filtered_df0 en rouge et filtered_df1 en bleu 
+# Créer une seule carte avec filtered_dg en rouge et filtered_df en bleu 
 st.markdown(f"<h2 style='font-size:22px;'> Gun en bleu et geocodage en rouge pour l'intervalle {selected_interval_left} to {selected_interval_right} (ICPE tout type)</h2>", unsafe_allow_html=True)
 
 # Créer une seule carte avec filtered_df0 en rouge et filtered_df1 en bleu
 fig = px.scatter_mapbox(filtered_dg1, lat="latitude", lon="longitude", hover_data=["Nom_usuel", "Code_AIOT", "Adresse_si","nb_points"], size_max=20, zoom=8, color_discrete_sequence=['red'])
-fig.add_trace(px.scatter_mapbox(filtered_df, lat="latitude", lon="longitude", hover_data=["Nom_usuel", "Code_AIOT","Adresse_concat","nb_points"], size_max=20, color_discrete_sequence=['blue']).data[0])
+fig.add_trace(px.scatter_mapbox(filtered_df, lat="latitude", lon="longitude", hover_data=["Nom_usuel", "Code_AIOT","Adresse_concat","nb_points"], size_max=20, color_discrete_sequence=['blue']).data[0],opacity=0.7)
 
 fig.update_layout(mapbox_style="open-street-map")
 fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
