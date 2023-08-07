83eb7627645a0f0e6dd8211543af939472627460
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-07 19:36:21 +0200

    Update distance8.py

--- distance8.py
+++ distance8.py
@@ -193,7 +193,7 @@ center_lat = filtered_dg1['latitude'].mean()
 center_lon = filtered_dg1['longitude'].mean()
 
 # Créer une seule carte avec filtered_df0 en rouge et filtered_df1 en bleu 
-st.markdown("<h2 style='font-size:22px;'> gun en bleu et geocodage en rouge pour l'intervalle choisi</h2>", unsafe_allow_html=True)
+st.markdown("<h2 style='font-size:22px;'> gun en bleu et geocodage en rouge pour l'intervalle {selected_interval_left} to {selected_interval_right} (ICPE tout type)</h2>", unsafe_allow_html=True)
 
 
 # Créer une seule carte avec filtered_df0 en rouge et filtered_df1 en bleu
