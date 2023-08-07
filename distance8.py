e5de1545067490a8eb94fcba5dfaa962c1d688a4
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-07 11:58:37 +0200

    Update distance8.py

--- distance8.py
+++ distance8.py
@@ -183,7 +183,7 @@ center_lat = filtered_df['latitude'].mean()
 center_lon = filtered_df['longitude'].mean()
 
 # Créer une seule carte avec filtered_df en rouge et filtered_df1 en bleu
-
+st.markdown("<h2 style='font-size:22px;'> gun en bleu et geocodage en rouge pour l'intervalle chosi</h2>", unsafe_allow_html=True)
 
 fig = px.scatter_mapbox(filtered_df, lat="latitude", lon="longitude", hover_data=["Nom_usuel", "Code_AIOT"], size='nb_points', size_max=20, zoom=10, color_discrete_sequence=['red'])
 fig.add_trace(px.scatter_mapbox(filtered_df1, lat="latitude", lon="longitude", hover_data=["Nom_usuel", "Code_AIOT"], size='nb_points', size_max=10, color_discrete_sequence=['blue']).data[0])
