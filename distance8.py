5432a86db4231bc9aee3be88c7489d0826f07daf
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-08 12:48:07 +0200

    Update distance8.py

--- distance8.py
+++ distance8.py
@@ -4,7 +4,6 @@ import numpy as np
 import base64
 import plotly.express as px
 
-
 # Load data from  CSV
 dg = pd.read_csv('https://raw.githubusercontent.com/olivierparrot01/ICPE/main/2geocodage.csv')
 df = pd.read_csv('https://raw.githubusercontent.com/olivierparrot01/ICPE/main/0208_gun.csv')
@@ -232,52 +231,24 @@ filtered_df2["Nom_usuel_liste"] = filtered_df2.groupby(["latitude", "longitude"]
 center_lat = filtered_dg2['latitude'].mean()
 center_lon = filtered_dg2['longitude'].mean()
 
-import streamlit as st
-import pandas as pd
-import plotly.express as px
-
-# Supposons que filtered_dg2 et filtered_df2 soient vos DataFrames avec les données
-
-# Calculer les coordonnées moyennes des latitudes et longitudes de filtered_dg2
-center_lat = filtered_dg2['latitude'].mean()
-center_lon = filtered_dg2['longitude'].mean()
-
 # Créer la carte avec des points rouges (dg) et bleus (df)
 fig = px.scatter_mapbox(filtered_dg2, lat="latitude", lon="longitude", hover_data=["Nom_usuel_liste", "Code_AIOT_liste", "Adresse_si", "nb_points"], size='nb_points', size_max=10, zoom=8, color_discrete_sequence=['red'])
 fig.add_trace(px.scatter_mapbox(filtered_df2, lat="latitude", lon="longitude", hover_data=["Nom_usuel_liste", "Code_AIOT_liste", "Adresse_concat", "nb_points"], size='nb_points', size_max=10, color_discrete_sequence=['blue']).data[0])
 
-# Définir la fonction de calcul de distance
-def calculate_distance(lat1, lon1, lat2, lon2):
-    return geodesic((lat1, lon1), (lat2, lon2)).kilometers
-
-# Mettre à jour la mise en page de la carte avec hovertemplate
-fig.update_traces(hovertemplate='%{customdata[0]}<br>%{customdata[1]}<br>%{customdata[2]}<br>Distance : %{customdata[3]:.2f} km')
-
-# Ajouter les données de distance personnalisées
-for index, row in filtered_dg2.iterrows():
-    fig.add_trace(go.Scattermapbox(
-        lat=[row['latitude']],
-        lon=[row['longitude']],
-        customdata=[row['Nom_usuel_liste'], row['Code_AIOT_liste'], row['Adresse_si'], 0],  # Distance initialisée à 0
-        mode='markers',
-        marker=dict(size=10, color='red'),
-        hoverinfo='skip'  # Pour éviter l'affichage double des informations au survol
-    ))
-
-for index, row in filtered_df2.iterrows():
-    fig.add_trace(go.Scattermapbox(
-        lat=[row['latitude']],
-        lon=[row['longitude']],
-        customdata=[row['Nom_usuel_liste'], row['Code_AIOT_liste'], row['Adresse_concat'], 0],  # Distance initialisée à 0
-        mode='markers',
-        marker=dict(size=10, color='blue'),
-        hoverinfo='skip'  # Pour éviter l'affichage double des informations au survol
-    ))
-
-# Mettre à jour la mise en page de la carte
 fig.update_layout(mapbox_style="open-street-map")
 fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 fig.update_layout(mapbox_center={"lat": center_lat, "lon": center_lon})
+
+
+with st.expander(f"Afficher les données Gun"):
+    # Afficher la table à l'intérieur de la section expansible
+    st.dataframe(filtered_df2)
+
+with st.expander(f"Afficher les données Geocodage"):
+    # Afficher la table à l'intérieur de la section expansible
+    st.dataframe(filtered_dg2)
+
+
 
 # Afficher la carte dans Streamlit 
 st.plotly_chart(fig)
