75098352cc79def71c3c60bc57f3a4075a535d9f
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-08 12:46:29 +0200

    Update distance8.py

--- distance8.py
+++ distance8.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import base64
 import plotly.express as px
-from geopy.distance import geodesic
+
 
 # Load data from  CSV
 dg = pd.read_csv('https://raw.githubusercontent.com/olivierparrot01/ICPE/main/2geocodage.csv')
