a9e3ee8d1b5c32ee4162ee2c9078103f31d27bfd
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-09 11:33:13 +0200

    Update distance8.py

--- distance8.py
+++ distance8.py
@@ -320,5 +320,5 @@ def create_folium_map_with_scale_bar(center_lat, center_lon, data):
 center_lat = 48.8566
 center_lon = 2.3522
 filtered_dg1 = None  # Replace with your data (filtered DataFrame)
-folium_map_html = create_folium_map_with_scale_bar(center_lat, center_lon, filtered_dg1)
+folium_map_html = create_folium_map_with_scale_bar(center_lat, center_lon, dg)
 st.components.v1.html(folium_map_html, height=600)
