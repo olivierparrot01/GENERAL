7c72e64e00af5d8758b0b56adbff0bc932f8b7a1
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-10 15:05:08 +0200

    Update distance7.py

--- distance7.py
+++ distance7.py
@@ -412,9 +412,9 @@ st.dataframe(filtered_data)
 
 
 # Afficher les adresses Gun des points sélectionnés
-st.write("Adresses Gun des points sélectionnés et cordonnées Gun des points sélectionnés :")
+st.write("Adresses et coordonnées Gun des points sélectionnés :")
 for _, row in filtered_data.iterrows():
-    st.write(f"Adresse Gun: {row['Adresse_concat']}, Coordonnées Gun: {row['longitude']}, {row['latitude']}")
+    st.write(f"Adresse Gun : {row['Adresse_concat']}, Coordonnées Gun : {row['longitude']}, {row['latitude']}")
     formatted_address = row['Adresse_concat'].replace(' ', '-')
     google_maps_link = f"[Ouvrir dans Google Maps à partir de l'adresse Gun](https://www.google.com/maps/search/?api=1&query={formatted_address})"
     st.markdown(google_maps_link, unsafe_allow_html=True)
