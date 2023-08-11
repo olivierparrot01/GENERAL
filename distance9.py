278ba60150da27566468f59ad09e2b11bafcc7e4
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-11 16:27:25 +0200

    Update distance9.py

--- distance9.py
+++ distance9.py
@@ -333,7 +333,7 @@ for _, row in filtered_dg1.iterrows():
         fill=True,
         fill_color='red',
         fill_opacity=0.6,
-        popup_content = f"Nom usuel : {row['Nom_usuel']}<br>Code AIOT : {row['Code_AIOT_liste']}"
+        popup_content = f"Nom usuel : {row['Nom_usuel']}<br>Code AIOT : {row['Code_AIOT_liste']}",
         tooltip_content = f"Nom usuel : {row['Nom_usuel']}<br>Code AIOT : {row['Code_AIOT_liste']}"
     ).add_to(m)
 
