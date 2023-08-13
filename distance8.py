fe46d5dbbb873516075219017c987803b1a3d0c4
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-13 18:10:33 +0200

    Update distance8.py

--- distance8.py
+++ distance8.py
@@ -101,5 +101,26 @@ geojson_layer.add_to(m)
 # Afficher la carte dans Streamlit en utilisant folium_static
 folium_static(m)
 
+st.markdown("<h2 style='font-size:18px;'>Sélectionner par le Code AIOT les points Gun à mettre en évidence (carte, table et liens Google Maps)</h2>", unsafe_allow_html=True)
+# Sélection des codes AIOT à mettre en évidence
+selected_codes = st.multiselect( "",    df)
 
+
+# Filtrer les données en fonction des codes AIOT sélectionnés
+filtered_data = df[df['Code_AIOT_liste'].isin(selected_codes)]
+
+# Mettre en évidence les points correspondant aux codes AIOT sélectionnés
+for index, row in filtered_data.iterrows():
+    folium.CircleMarker(
+        location=[row['latitude'], row['longitude']],
+        radius=10,
+        color='green',
+        fill=True,
+        fill_color='green',
+        fill_opacity=0.6,
+        popup=f"Code_AIOT(S): {row['Code_AIOT_liste']}"
+    ).add_to(m)
+
+# Afficher la carte mise à jour dans Streamlit en utilisant folium_static
+folium_static(m)
 
