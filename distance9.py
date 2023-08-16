3bcb0c611560ef9d4b74d66724cc13166995e4d9
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-16 19:37:15 +0200

    Update distance9.py

--- distance9.py
+++ distance9.py
@@ -192,25 +192,11 @@ for zoom_level in range(13, 19):
         max_zoom=zoom_level,
     ).add_to(m)
 
-# Initialiser st.session_state
-if 'click_data' not in st.session_state:
-    st.session_state.click_data = None
-
-# Fonction pour gérer le clic sur la carte
-def handle_click(lat, lon):
-    st.session_state.click_data = {'lat': lat, 'lon': lon}
-
 
 # Afficher la carte dans Streamlit en utilisant folium_static
 folium_static(m)
 
 
-# Ajouter un événement de clic pour capturer les coordonnées
-if st.session_state.click_data:
-    st.sidebar.write(f"Latitude : {st.session_state.click_data['lat']}, Longitude : {st.session_state.click_data['lon']}")
-
-# Ajouter l'événement de clic à la carte
-st.map.handle_click(m, handle_click)
 
 st.markdown("<h2 style='font-size:18px;'>Adresses, coordonnées Gun et liens Google Maps des points sélectionnés : </h2>", unsafe_allow_html=True)
 
