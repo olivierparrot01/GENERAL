f4a4a5652dce70335af0a5271d6aec6107290e70
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-17 01:45:01 +0200

    Update distance6.py

--- distance6.py
+++ distance6.py
@@ -2,11 +2,21 @@ import streamlit as st
 import folium
 from streamlit_folium import folium_static
 
+# Create a Streamlit app
+st.title("Coordinate Capture with Folium in Streamlit")
+
 # Create a map using Folium
 m = folium.Map(location=[48.8566, 2.3522], zoom_start=12)
 
 # Add a LatLngPopup to the map
-folium.LatLngPopup().add_to(m)
+popup = folium.LatLngPopup()
+m.add_child(popup)
 
-# Display the map in Streamlit
+# Display the map in Streamlit using folium_static
 folium_static(m)
+
+# Display the captured coordinates in Streamlit
+if popup:
+    st.write("Captured Coordinates:")
+    st.write("Latitude:", popup.args[0].lat)
+    st.write("Longitude:", popup.args[0].lng)
