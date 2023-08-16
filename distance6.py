a2c3f5b783823cdcf50c224bf4ccfc289f593c75
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-17 01:05:57 +0200

    Update distance6.py

--- distance6.py
+++ distance6.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import folium
 from streamlit_folium import folium_static
-import folium.plugins as plugins
 
 # Créer une carte avec Folium
 m = folium.Map(location=[48.8566, 2.3522], zoom_start=12)
@@ -9,14 +8,22 @@ m = folium.Map(location=[48.8566, 2.3522], zoom_start=12)
 # Utiliser folium_static pour afficher la carte dans Streamlit
 folium_static(m)
 
-# Fonction pour gérer le clic sur la carte
-def handle_click(event, **kwargs):
-    lat, lon = event.latlng
-    st.write(f"Latitude : {lat}, Longitude : {lon}")
+# Liste pour stocker les coordonnées des clics
+click_coordinates = []
+
+# Ajouter une zone de texte pour afficher les coordonnées capturées
+st.sidebar.subheader("Coordonnées capturées")
+coordinates_text = st.sidebar.empty()
+
+# Fonction pour mettre à jour la liste des coordonnées et le texte
+def update_coordinates(lat, lon):
+    click_coordinates.append((lat, lon))
+    coordinates_text.text(f"Latitude : {lat}, Longitude : {lon}")
 
 # Ajouter l'événement de clic à la carte
-m.add_child(folium.ClickForMarker(callback=handle_click))
+m.add_child(folium.ClickForMarker(popup=None, callback=update_coordinates))
 
-# Afficher la carte mise à jour dans Streamlit en utilisant folium_static
-folium_static(m)
-
+# Afficher les coordonnées capturées
+st.sidebar.write("Coordonnées capturées :")
+for lat, lon in click_coordinates:
+    st.sidebar.write(f"Latitude : {lat}, Longitude : {lon}")
