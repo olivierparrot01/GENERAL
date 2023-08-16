10d31ba5ae13fb7f4f12ade496342b763bc50e3f
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-17 01:38:14 +0200

    Update distance6.py

--- distance6.py
+++ distance6.py
@@ -2,28 +2,11 @@ import streamlit as st
 import folium
 from streamlit_folium import folium_static
 
-# Créer une carte avec Folium
+# Create a map using Folium
 m = folium.Map(location=[48.8566, 2.3522], zoom_start=12)
 
-# Utiliser folium_static pour afficher la carte dans Streamlit
+# Add a LatLngPopup to the map
+folium.LatLngPopup().add_to(m)
+
+# Display the map in Streamlit
 folium_static(m)
-
-# Liste pour stocker les coordonnées des clics
-click_coordinates = []
-
-# Ajouter une zone de texte pour afficher les coordonnées capturées
-st.sidebar.subheader("Coordonnées capturées")
-coordinates_text = st.sidebar.empty()
-
-# Fonction pour mettre à jour la liste des coordonnées et le texte
-def update_coordinates(lat, lon):
-    click_coordinates.append((lat, lon))
-    coordinates_text.text(f"Latitude : {lat}, Longitude : {lon}")
-
-# Ajouter l'événement de clic à la carte
-m.add_child(folium.ClickForMarker(popup=None, callback=update_coordinates))
-
-# Afficher les coordonnées capturées
-st.sidebar.write("Coordonnées capturées :")
-for lat, lon in click_coordinates:
-    st.sidebar.write(f"Latitude : {lat}, Longitude : {lon}")
