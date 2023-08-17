c8dd060824c2d1805a2b912717f326de5513e5c0
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-17 11:43:55 +0200

    Update distance9.py

--- distance9.py
+++ distance9.py
@@ -109,7 +109,8 @@ st.markdown("<h2 style='font-size:28px;'>Appareillement Gun-Geocodage et recherc
 #st.write("Afficher/Masquer")
 #st.dataframe(df)
 
-
+# Sidebar pour les options et l'affichage des données
+st.sidebar.title("Options")
 
 # Afficher le titre dans le sidebar
 st.sidebar.markdown("<h2 style='font-size:18px;'>Afficher la table Gun</h2>", unsafe_allow_html=True)
@@ -143,13 +144,11 @@ lines_geojson_layer.add_to(m)
 
 
 # Filtrer les données en fonction des codes AIOT sélectionnés
-st.markdown("<h2 style='font-size:18px;'>Sélectionner par le Code AIOT les points Gun à mettre en évidence sur la carte<br>Sélection multiple, liens Google Maps individualisés</h2>", unsafe_allow_html=True)
+st.sidebar.markdown("<h2 style='font-size:18px;'>Sélectionner par le Code AIOT les points Gun à mettre en évidence sur la carte<br>Sélection multiple, liens Google Maps individualisés</h2>", unsafe_allow_html=True)
 # Triez les codes AIOT dans l'ordre décroissant
 sorted_codes = sorted(df['Code_AIOT'].unique(), reverse=True)
 
-
-selected_codes = st.multiselect("", sorted_codes)
-
+selected_codes = st.sidebar.multiselect("", sorted_codes)
 
 filtered_data = df[df['Code_AIOT'].isin(selected_codes)]
 
