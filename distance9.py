64edeb8fab99de0f4ecb7140883d1c6ff728b21b
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-20 20:07:54 +0200

    Update distance9.py

--- distance9.py
+++ distance9.py
@@ -228,7 +228,7 @@ for criterion in selected_criteria:
 
         # Add a button to filter distance none
         if st.sidebar.button("Filtrer pts Gun non geocodes"):
-            filtered_df = filtered_df[(filtered_df['Distance'].isna()]
+            filtered_df = filtered_df[filtered_df['Distance'].isna()]
         else:
             filtered_df = filtered_df[filtered_df['Distance'] >= selected_distance]
 
