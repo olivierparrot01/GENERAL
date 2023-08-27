319511f8b98a657ee953c24a8ed82d0d5ce9b575
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-27 22:26:44 +0200

    Update Cut.py

--- Cut.py
+++ Cut.py
@@ -10,8 +10,8 @@ gdf_i = gpd.read_file('https://raw.githubusercontent.com/olivierparrot01/ICPE/ma
 gdf_i = gdf_i.to_crs('EPSG:2154')
 
 gdf_pts = gpd.read_file('https://raw.githubusercontent.com/olivierparrot01/ICPE/main/g_pt3.shp', crs='EPSG:2154')
-#gdf_pts=gdf_pts.to_crs('EPSG:2154')
-st.dataframe(gdf_pts)
+gdf_pts=gdf_pts.to_crs('EPSG:2154')
+#st.dataframe(gdf_pts)
 
 # Coordonnées du centre de la région PACA
 center_lat = 43.7157
@@ -21,13 +21,13 @@ center_lon = 5.0792
 m = folium.Map(location=[center_lat, center_lon], zoom_start=8)  # Coordonnées et niveau de zoom pour la région PACA
 
 # Ajouter les lignes rouges à la carte
-folium.GeoJson(gdf_lignes, style_function=lambda x: {'color': 'red'}).add_to(m)
+#folium.GeoJson(gdf_lignes, style_function=lambda x: {'color': 'red'}).add_to(m)
 
 # Ajouter les lignes bleues à la carte
-folium.GeoJson(gdf_i, style_function=lambda x: {'color': 'blue'}).add_to(m)
+#folium.GeoJson(gdf_i, style_function=lambda x: {'color': 'blue'}).add_to(m)
 
 # Ajouter les points 
-#folium.GeoJson(gdf_pts).add_to(m)
+folium.GeoJson(gdf_pts).add_to(m)
 
 
 # Afficher la carte
