6b717b7c63d0f4a286237fbc825332c186ba0a7f
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-08-28 00:25:39 +0200

    Update Cut.py

--- Cut.py
+++ Cut.py
@@ -1,6 +1,5 @@
 import folium
 import geopandas as gpd
-from shapely.geometry import Point, LineString
 import streamlit as st
 from streamlit_folium import folium_static
 
@@ -9,9 +8,7 @@ gdf_lignes = gpd.read_file('https://raw.githubusercontent.com/olivierparrot01/IC
 gdf_i = gpd.read_file('https://raw.githubusercontent.com/olivierparrot01/ICPE/main/i_83.shp')
 gdf_i = gdf_i.to_crs('EPSG:2154')
 
-gdf_pts = gpd.read_file('https://raw.githubusercontent.com/olivierparrot01/ICPE/main/i_endpoints.shp', crs='EPSG:2154')
-gdf_pts=gdf_pts.to_crs('EPSG:2154')
-#st.dataframe(gdf_pts)
+gdf_pts = gpd.read_file('https://raw.githubusercontent.com/olivierparrot01/ICPE/main/g_pt3.shp', crs='EPSG:2154')
 
 # Coordonnées du centre de la région PACA
 center_lat = 43.7157
@@ -20,25 +17,25 @@ center_lon = 5.0792
 # Créer une carte Folium centrée sur la région d'intérêt
 m = folium.Map(location=[center_lat, center_lon], zoom_start=8)  # Coordonnées et niveau de zoom pour la région PACA
 
-# Ajouter les points 
-#folium.GeoJson(gdf_pts,style_function=lambda x: {'color': 'black'}).add_to(m)
-# Ajouter une couche de points à la carte
+# Ajouter les lignes à la carte
+folium.GeoJson(gdf_lignes).add_to(m)
+
+# Ajouter les lignes à la carte
+folium.GeoJson(gdf_i).add_to(m)
+
+# Créer une nouvelle carte pour les points
+m_pts = folium.Map(location=[center_lat, center_lon], zoom_start=8)  # Coordonnées et niveau de zoom pour la région PACA
+
+# Ajouter les points à la nouvelle carte avec des marqueurs noirs
 for idx, row in gdf_pts.iterrows():
     # Obtenez les coordonnées du point
     lat, lon = row['geometry'].y, row['geometry'].x
-    #st.write(lon)
-    # Créez un marqueur pour le point et ajoutez-le à la carte
-    folium.CircleMarker(location=[lat, lon], radius=16, color='black').add_to(m)
+    
+    # Créez un marqueur pour le point avec une couleur noire
+    folium.CircleMarker(location=[lat, lon], radius=16, color='black').add_to(m_pts)
 
-#folium.CircleMarker(location=[lat, lon], radius=16, color='black').add_to(m)
-#folium.GeoJson(gdf_pts, style_function=lambda x: {'color': 'yellow'}).add_to(m)
-
-
-# Ajouter les lignes rouges à la carte
-folium.GeoJson(gdf_lignes, style_function=lambda x: {'color': 'red'}).add_to(m)
-
-# Ajouter les lignes bleues à la carte
-folium.GeoJson(gdf_i, style_function=lambda x: {'color': 'blue'}).add_to(m)
-
-# Afficher la carte
+# Afficher la carte des lignes
 folium_static(m)
+
+# Afficher la carte des points
+folium_static(m_pts)
