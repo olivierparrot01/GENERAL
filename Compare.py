fd502f63aee3f8ffb876119386732a1c3df574a7
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-09-03 19:09:10 +0200

    Update Compare.py

--- Compare.py
+++ Compare.py
@@ -94,10 +94,7 @@ parties_communes = pd.merge(c,d,left_index=True, right_index=True, how='inner')
 
 # Afficher les parties communes
 st.write('en commun')
-        
-        
-        
-    st.dataframe   ( parties_communes)
+st.dataframe   ( parties_communes)
         
         
 
