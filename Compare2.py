9922dcef2d273b9b488042af5447ff88c19353f7
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-09-09 16:33:50 +0200

    Update Compare2.py

--- Compare2.py
+++ Compare2.py
@@ -5,6 +5,16 @@ from streamlit_folium import folium_static
 # Charger le GeoDataFrame à partir du fichier shapefile
 #gdf = gpd.read_file('nom_du_fichier_shapefile.shp')
 gdf = gpd.read_file('https://raw.githubusercontent.com/olivierparrot01/ICPE/main/i_83_topage.shp')
+
+
+# Convertir en WGS 84 (EPSG 4326)
+gdf= gdf.to_crs(epsg=4326)
+
+gdf= gdf.__geo_interface__
+# Maintenant, gdf_wgs84 contient les coordonnées en WGS 84
+
+
+
 # Créer une carte Folium
 m = folium.Map(location=[48.8566, 2.3522], zoom_start=12)
 
