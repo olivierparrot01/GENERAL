1359318cdab225b80bc98444d136ed286c09c099
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-09-20 16:37:26 +0200

    Update graphique_defrich.py

--- graphique_defrich.py
+++ graphique_defrich.py
@@ -16,16 +16,26 @@ gdf = gpd.read_file('https://raw.githubusercontent.com/olivierparrot01/ICPE/main
 
 # Prétraiter les données pour extraire la colonne "DATE_AP" et "S_DEFRICH"
 gdf['DATE_AP'] = pd.to_datetime(gdf['DATE_AP'])
+gdf['Année'] = gdf['DATE_AP'].dt.year
+
+# Agréger les données par année en calculant la somme de "S_DEFRICH"
+donnees_aggregatees = gdf.groupby('Année')['S_DEFRICH'].sum().reset_index()
 
 # Créer une application Streamlit
-st.title("Évolution de S_DEFRICH depuis 2014")
+st.title("Évolution de la somme de S_DEFRICH par année")
 
-# Créer un graphique interactif avec Seaborn pour toutes les années
+# Créer un graphique interactif avec Seaborn (courbe)
 plt.figure(figsize=(10, 6))
-sns.lineplot(data=gdf, x='DATE_AP', y='S_DEFRICH', estimator=None)
+sns.lineplot(data=donnees_aggregatees, x='Année', y='S_DEFRICH', marker='o')
 plt.xlabel("Année")
-plt.ylabel("S_DEFRICH")
-plt.title("Évolution de S_DEFRICH pour toutes les années")
+plt.ylabel("Somme de S_DEFRICH")
+plt.title("Somme de S_DEFRICH par année")
 plt.xticks(rotation=45)
 st.pyplot()
+Ce code utilise sns.lineplot de Seaborn pour créer une courbe montrant l'évolution de la somme de "S_DEFRICH" par année. Vous pouvez exécuter votre application Streamlit avec streamlit run nom_de_votre_script.py pour visualiser la courbe interactive.
+
+
+
+
+
 
