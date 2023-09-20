836855faaefa6ce7f9fb56f5361f6cd6414ae30c
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-09-20 15:43:08 +0200

    Update graphique_defrich.py

--- graphique_defrich.py
+++ graphique_defrich.py
@@ -12,7 +12,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Chargement du fichier shapefile
-gdf = gpd.read_file(r'V:\CONSULTATION\AMENAGEMENT_URBANISME\N_ZONAGES_AMENAGEMENT\AVIS_AE\PROJET\Cas_par_cas_Projet_defrichement.shp')
+gdf = gpd.read_file('https://raw.githubusercontent.com/olivierparrot01/ICPE/main/Cas_par_cas_Projet_defrichement.shp')
+
 
 # Conversion du champ "DATE_AP" en format de date
 gdf['DATE_AP'] = pd.to_datetime(gdf['DATE_AP'])
