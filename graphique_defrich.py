501e4a02ac7c29e4161e0ecae3e0dbce9533d4f4
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2023-09-21 11:09:51 +0200

    Update graphique_defrich.py

--- graphique_defrich.py
+++ graphique_defrich.py
@@ -15,7 +15,15 @@ import plotly.express as px
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 # Chargement du fichier shapefile
-gdf = gpd.read_file('https://raw.githubusercontent.com/olivierparrot01/ICPE/main/Cas_par_cas_Projet_defrichement.shp')
+gdf = gpd.read_file(r'V:\CONSULTATION\AMENAGEMENT_URBANISME\N_ZONAGES_AMENAGEMENT\AVIS_AE\PROJET\Cas_par_cas_Projet_defrichement.shp')
+
+
+gdf1 = gpd.read_file(r'S:\1_SIG\1_REFERENTIEL\BDCARTO_IGN\ADMINISTRATIF\COMMUNE.shp')
+
+
+# Joindre les GeoDataFrames en utilisant la colonne 'NOM_COM'
+gdf = gdf.merge(gdf1[['NOM_COMM_M', 'INSEE_DEP']], left_on='LOCALITE',right_on='NOM_COMM_M', how='left')
+
 
 # Conversion du champ "DATE_AP" en format de date
 gdf['DATE_AP'] = pd.to_datetime(gdf['DATE_AP'])
@@ -35,10 +43,12 @@ annees_personnalisees = list(range(2014, 2024, 1))
 
 # Création de la liste d'années personnalisée
 
+st.title("Évolution du Total défrichement régional")
+
 
 fig = px.line(donnees_aggregatees, x='ANNEE', y='S_DEFRICH', markers=True)
 fig.update_traces(mode='markers+lines', hovertemplate="Année: %{x}<br>Somme: %{y}")
-fig.update_layout(xaxis_title="Année", yaxis_title="Somme de S_DEFRICH")
+fig.update_layout(xaxis_title="Année", yaxis_title="Total défrichement en m2")
 
 # Définir manuellement les valeurs de l'axe des x (toutes les années)
 fig.update_xaxes(tickvals=annees_personnalisees, ticktext=annees_personnalisees)
@@ -54,36 +64,69 @@ st.plotly_chart(fig)
 
 
 
+
+# # Créer une application Streamlit
+# st.title("Évolution de la somme de S_DEFRICH par année et par commune")
+# # Calculer la somme de S_DEFRICH par commune
+# somme_defrich_commune = gdf.groupby('LOCALITE')['S_DEFRICH'].sum().reset_index()
+
+# # Trier les communes par somme de S_DEFRICH décroissante
+# somme_defrich_commune = somme_defrich_commune.sort_values(by='S_DEFRICH', ascending=False)
+
+# # Sélectionner une commune à partir d'une liste déroulante triée
+# communes = somme_defrich_commune['LOCALITE'].tolist()
+
+# commune_selectionnee = st.selectbox("Sélectionnez une commune", communes)
+
+# # Filtrer les données en fonction de la commune sélectionnée
+# donnees_commune = gdf[gdf['LOCALITE'] == commune_selectionnee]
+
+# # Agréger les données par année pour la commune sélectionnée
+# donnees_aggregatees_commune = donnees_commune.groupby('ANNEE')['S_DEFRICH'].sum().reset_index()
+
+# # Créer un graphique interactif avec Plotly (courbe)
+# fig = px.line(donnees_aggregatees_commune, x='ANNEE', y='S_DEFRICH', markers=True)
+# fig.update_traces(mode='markers+lines', hovertemplate="Année: %{x}<br>Somme: %{y}")
+# fig.update_layout(xaxis_title="Année", yaxis_title="Somme de S_DEFRICH")
+
+# # Définir manuellement les valeurs de l'axe des x (toutes les années)
+# fig.update_xaxes(tickvals=annees_personnalisees, ticktext=annees_personnalisees)
+
+# # Afficher le graphique interactif
+# st.plotly_chart(fig)
+
+
+
+
+# Charger le GeoDataFrame
+# gdf = gpd.read_file(r'V:\CONSULTATION\AMENAGEMENT_URBANISME\N_ZONAGES_AMENAGEMENT\AVIS_AE\PROJET\Cas_par_cas_Projet_defrichement.shp')
+
+# Conversion du champ "DATE_AP" en format de date
+gdf['DATE_AP'] = pd.to_datetime(gdf['DATE_AP'])
+
+# Création d'une colonne pour extraire l'année
+gdf['ANNEE'] = gdf['DATE_AP'].dt.year
+
+# Agréger les données par année et par commune (utilisant INSEE_DEP)
+donnees_aggregatees_commune = gdf.groupby(['ANNEE', 'INSEE_DEP'])['S_DEFRICH'].sum().reset_index()
 
 # Créer une application Streamlit
-st.title("Évolution de la somme de S_DEFRICH par année et par commune")
-# Calculer la somme de S_DEFRICH par commune
-somme_defrich_commune = gdf.groupby('LOCALITE')['S_DEFRICH'].sum().reset_index()
+st.title("Évolution du Total défrichement par département")
 
-# Trier les communes par somme de S_DEFRICH décroissante
-somme_defrich_commune = somme_defrich_commune.sort_values(by='S_DEFRICH', ascending=False)
+# Liste déroulante multisélection pour sélectionner les communes (INSEE_DEP)
+communes_selectionnees = st.multiselect("Sélectionner un département", donnees_aggregatees_commune['INSEE_DEP'].unique())
 
-# Sélectionner une commune à partir d'une liste déroulante triée
-communes = somme_defrich_commune['LOCALITE'].tolist()
-
-commune_selectionnee = st.selectbox("Sélectionnez une commune", communes)
-
-# Filtrer les données en fonction de la commune sélectionnée
-donnees_commune = gdf[gdf['LOCALITE'] == commune_selectionnee]
-
-# Agréger les données par année pour la commune sélectionnée
-donnees_aggregatees_commune = donnees_commune.groupby('ANNEE')['S_DEFRICH'].sum().reset_index()
+# Filtrer les données en fonction des communes sélectionnées
+donnees_filtrees = donnees_aggregatees_commune[donnees_aggregatees_commune['INSEE_DEP'].isin(communes_selectionnees)]
 
 # Créer un graphique interactif avec Plotly (courbe)
-fig = px.line(donnees_aggregatees_commune, x='ANNEE', y='S_DEFRICH', markers=True)
+fig = px.line(donnees_filtrees, x='ANNEE', y='S_DEFRICH', color='INSEE_DEP', markers=True)
 fig.update_traces(mode='markers+lines', hovertemplate="Année: %{x}<br>Somme: %{y}")
-fig.update_layout(xaxis_title="Année", yaxis_title="Somme de S_DEFRICH")
-
-# Définir manuellement les valeurs de l'axe des x (toutes les années)
+fig.update_layout(xaxis_title="Année", yaxis_title="Total défrichement en m2")
 fig.update_xaxes(tickvals=annees_personnalisees, ticktext=annees_personnalisees)
-
 # Afficher le graphique interactif
 st.plotly_chart(fig)
+
 
 
 
