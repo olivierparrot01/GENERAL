a9838b64f016927143750ac55e2516655a40014e
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2024-01-26 11:23:43 +0100

    Update graphique_defrich.py

--- graphique_defrich.py
+++ graphique_defrich.py
@@ -16,7 +16,7 @@ st.set_option('deprecation.showPyplotGlobalUse', False)
 # Chargement du fichier shapefile
 gdf = gpd.read_file('https://raw.githubusercontent.com/olivierparrot01/ICPE/main/Cas_par_cas_Projet_defrichement.shp')
 
-#♠gdf = gpd.read_file(r'V:\CONSULTATION\AMENAGEMENT_URBANISME\N_ZONAGES_AMENAGEMENT\AVIS_AE\PROJET\Cas_par_cas_Projet_defrichement.shp')
+#gdf = gpd.read_file(r'V:\CONSULTATION\AMENAGEMENT_URBANISME\N_ZONAGES_AMENAGEMENT\AVIS_AE\PROJET\Cas_par_cas_Projet_defrichement.shp')
 
 def func(x):
     
