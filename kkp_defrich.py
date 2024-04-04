437ce7f83c5724c48fd8dc08978d38a9538afe72
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2024-04-04 17:55:56 +0200

    Update kkp_defrich.py

--- kkp_defrich.py
+++ kkp_defrich.py
@@ -98,7 +98,7 @@ donnees_aggregatees_depart = gdf.groupby(['ANNEE', 'INSEE_DEP'])['S_DEFRICH'].su
 st.header("Évolution du défrichement par département")
 st.write("\n")
 # Liste déroulante multisélection pour sélectionner les communes (INSEE_DEP)
-st.sidebar.subheader("Comparer des départements (sélection multiple")
+st.sidebar.subheader("Comparer des départements (sélection multiple)")
 departements_selectionnees = st.sidebar.multiselect("", donnees_aggregatees_depart['INSEE_DEP'].unique())
 
 # Filtrer les données en fonction des communes sélectionnées
