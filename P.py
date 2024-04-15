582a906399fcf6c99572248d338535118060f330
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2024-04-16 00:34:07 +0200

    Update P.py

--- P.py
+++ P.py
@@ -74,7 +74,7 @@ for year in selected_years:
 
     #fig = px.pie(grouped_data, values='COUNT', names='CATEGORIE', title=f'Répartition des projets par catégorie pour l\'année {year}', color=categories, color_discrete_map=category_colors)
 
-    fig = px.pie(grouped_data, values='COUNT', names='CATEGORIE', title=f'Répartition des {total_projects} projets par catégorie pour l\'année {year}', color='CATEGORIE', color_discrete_map=category_color_map,hover_data={'LOCALITE': True, 'PROJET': True})
+    fig = px.pie(grouped_data, values='COUNT', names='CATEGORIE', title=f'Répartition des {total_projects} projets par catégorie pour l\'année {year}', color='CATEGORIE', color_discrete_map=category_color_map)
     #fig = px.pie(grouped_data, values='COUNT', names='CATEGORIE', title=f'Répartition des projets par catégorie pour l\'année {year}', color='CATEGORIE', color_discrete_sequence=colors)
     # Afficher uniquement le nombre de projets dans le camembert
     fig.update_traces(text=grouped_data['COUNT'], textposition='inside', insidetextfont=dict(size=16))
