fa10e243e97b110e9574391e55f34b793a45ad06
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2024-07-26 12:11:00 +0200

    Update P.py

--- P.py
+++ P.py
@@ -88,7 +88,7 @@ for year in selected_years:
     #fig.update_traces( text=grouped_data['COUNT'], textposition='inside', insidetextfont=dict(size=16))
 
     # Forcer les pourcentages à être entiers
-    fig.update_traces(texttemplate='%{label}<br>%{value} (%{percent:.0%})')
+    #fig.update_traces(texttemplate='%{label}<br>%{value} (%{percent:.0%})')
 
    
     # Afficher le camembert
