22a5a041671c420625588164e751c485c7329a1a
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2024-08-31 22:10:25 +0200

    Update p_streamlit.py

--- p_streamlit.py
+++ p_streamlit.py
@@ -45,7 +45,7 @@ colors = [
 
 
 category_color_map = dict(zip(categories, colors))
-st.write(df['CATEGORIE'])
+st.write(list(df['CATEGORIE']))
 # Filtrage des données
 df = df[df['DATE_PUBLI'] >= '2015']
 df['DATE_PUBLI'] = pd.to_datetime(df['DATE_PUBLI'])
