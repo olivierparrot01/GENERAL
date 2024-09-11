bac133712f4f5d3ac499f78dd3f294ff6160e34b
olivierparrot01 <134491775+olivierparrot01@users.noreply.github.com>
2024-09-11 19:57:32 +0200

    Update p_streamlit.py

--- p_streamlit.py
+++ p_streamlit.py
@@ -322,10 +322,13 @@ color_map = {
 # Coordonnées approximatives du centre de la région PACA
 latitude_center = 43.5  # Latitude centrale de PACA
 longitude_center = 6.5  # Longitude centrale de PACA
-
+# Vérifiez les valeurs moyennes
+mean_latitude = filtered_df['latitude'].mean()
+mean_longitude = filtered_df['longitude'].mean()
 
 # Créer la carte centrée sur le centre des données
-m = folium.Map(location=[latitude_center, longitude_center], zoom_start=5)
+#m = folium.Map(location=[latitude_center, longitude_center], zoom_start=5)
+m = folium.Map(location=[mean_latitude, mean_longitude], zoom_start=5)
 
 
 # Ajouter le bouton de plein écran
